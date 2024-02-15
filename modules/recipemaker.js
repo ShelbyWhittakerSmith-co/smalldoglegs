f18ceacf0f95ecf7b5c39b0bb440fbdf2b9602ab
Lou <louwhi1992@gmail.com>
2024-02-15 11:10:13 -0700

    updates to formatting
    I adjusted the formatting so the response is less stupid.

--- modules/recipemaker.js
+++ modules/recipemaker.js
@@ -5,13 +5,14 @@ function recipeResults(results){
   param = document.getElementById('recipeName').appendChild(document.createElement('p'))
   for (x in results2){
     results3 = results2[x]
-
+    let ingredients = JSON.parse(JSON.parse(results3['Ingredients'])['Ingredients'])
+    let recipe_steps = JSON.parse(JSON.parse(results3['Steps'])['Steps'])
     buildElement("p", 'recipeName', x+'name')
-    document.getElementById(x+'name').innerHTML =  results3['Name']+  "  Ready In: " + results3['ReadyIn'] + "  Serves: " + results3['Serves'] +
     buildElement("p", 'recipeName', x+'ingredients' )
-    document.getElementById(x+'ingredients').innerHTML = results3['Ingredients']
     buildElement('p', 'recipeName', x+'Steps')
-    document.getElementById(x+'Steps').innerHTML = results3['Steps']
+    document.getElementById(x+'name').innerHTML = results3['Name']+  "<br>  Ready In: " + results3['ReadyIn'] + "<br>  Serves: " + results3['Serves']
+    document.getElementById(x+'ingredients').innerHTML =ingredients.join('<br>')
+    document.getElementById(x+'Steps').innerHTML = recipe_steps.join('<br>')
     grocery_list.push(results3['Ingredients']);
   }
   document.getElementById('recipeSteps').innerHTML = 'Grocery List: ' + results3['Ingredients']
@@ -38,5 +39,6 @@ function onRecipeClick(){
   buildElement('div', 'content_box', 'recipeName')
   buildElement('div', 'content_box', 'recipeSteps')
   dom_val = document.getElementById('loading').innerHTML = 'Loading...'
-  hitAPI("https://smalldoglegs.fly.dev/recipe/3"); 
+  hitAPI("https://smalldoglegs.fly.dev/recipe_id/2036")
+  // hitAPI("https://smalldoglegs.fly.dev/recipe/3"); 
 }
