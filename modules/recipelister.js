c87088bd3dace8370c8cdea57026078ff056c26c
GracefulDecay <137560473+GracefulDecay@users.noreply.github.com>
2024-06-25 18:50:15 -0600

    Assist (#33)
    * Cleaning up Recipe Maker and Shelby Info
    
    * 3
    
    * updates to formatting
    
    I adjusted the formatting so the response is less stupid.
    
    * oops
    
    oops
    
    * updates to make it feel slightly planned
    
    * format recipe page & fix grocery list
    
    * oops merge
    
    * I am so behind on commits. I fail. I know.
    
    ---------
    
    Signed-off-by: GracefulDecay <137560473+GracefulDecay@users.noreply.github.com>

--- modules/recipelister.js
+++ modules/recipelister.js
@@ -5,23 +5,16 @@ function recipeResults(results){
   param = document.getElementById('recipeName').appendChild(document.createElement('p'))
   for (x in results2){
     results3 = results2[x]
-    let ingredients = JSON.parse(JSON.parse(results3['Ingredients'])['Ingredients'])
-    let recipe_steps = JSON.parse(JSON.parse(results3['Steps'])['Steps'])
-    buildElement("h2", 'recipeName', x+'name')
-    buildElement("p", 'recipeName', x+'readyin')
-    buildElement("p", 'recipeName', x+'ingredients' )
-    buildElement('p', 'recipeName', x+'Steps')
-    document.getElementById(x+'name').innerHTML = results3['Name']
-    document.getElementById(x+'readyin').innerHTML = "Ready In: " + results3['ReadyIn'] + "<br>  Serves: " + results3['Serves']
-    document.getElementById(x+'ingredients').innerHTML =ingredients.join('<br>')
-    document.getElementById(x+'Steps').innerHTML = recipe_steps.join('<br>')
-    grocery_list = grocery_list.concat(ingredients);
-  }
-  document.getElementById('recipeSteps').innerHTML = 'Grocery List: ' + grocery_list
+    buildElement("a", 'recipeName', x+'name')
+    document.getElementById(x+'name').innerHTML = results3['Name'] + " " + results3['id'] + "<br>"
+    // document.getElementById(x+'name').addEventListener('click', () => {document.location.href = 'recipepager.html?recipe_id='+ results3['id']});
+    url = "recipepager.html?recipe_id=" + results3['id']
+    document.getElementById(x+'name').setAttribute('onclick', "window.open('"+url+"')")
+  };
+
   document.getElementById('loading').innerHTML = ''
   return 
 }
-
 
 function buildRecipePage(){
   const searchParams = new URLSearchParams(window.location.search);
@@ -40,6 +33,13 @@ async function hitAPI(url) {
   await recipeResults(myjson)
 }
 
+
+async function hitAPI(url) {
+  let myObject = await fetch(url);
+  let myjson = await myObject.text();
+  await recipeResults(myjson)
+}
+
 async function buildElement(type, parent, id){
   const par = document.getElementById(parent)
   const cor = document.createElement(type)
@@ -48,11 +48,18 @@ async function buildElement(type, parent, id){
   return 
 }
 
-function onRecipeClick(endpoint){
+function onRecipeListClick(endpoint){
   buildElement('div', 'content_box', 'recipeName')
   buildElement('div', 'content_box', 'recipeSteps')
   dom_val = document.getElementById('loading').innerHTML = 'Loading...'
-  // hitAPI("https://smalldoglegs.fly.dev/recipe_id/2036")
   url = "https://smalldoglegs.fly.dev/" + endpoint 
+  hitAPI(url); 
+}
+
+function onListResultClick(id){
+  buildElement('div', 'content_box', 'recipeName')
+  buildElement('div', 'content_box', 'recipeSteps')
+  dom_val = document.getElementById('loading').innerHTML = 'Loading...'
+  url = "https://smalldoglegs.fly.dev/recipe_id/" + id
   hitAPI(url); 
 }
